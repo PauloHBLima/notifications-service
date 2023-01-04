ac695c24a3253e75eab1153a91c447c05dd98f70
Paulo Henrique Barbosa De Lima <ph.blima30@gmail.comm>
2023-01-04 00:52:38 -0300

    Fundamentos do Nestjs, Prisma ORM

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto'
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) { }
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'você tem uma nova solicitação de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      }
+    })
+  }
+}
